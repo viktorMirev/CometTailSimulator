7816afe85e6c5bd927c600a80b6d1620c94d61b9
Viktor Mirev <viktor.mirev@gmail.com>
2018-03-13 20:24:24 +0200

    First

---
+++ CometTailSimulator/CometTailSimulator/Classes/Particle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CometTailSimulator.Classes
+{
+    public class Particle
+    {
+        private const int centralMassX = 0;
+        private const int centralMassY = 0;
+        private int x;
+        private int y;
+        private double speed;
+        private double angle;
+        private double size;
+
+        public void Update()
+        {
+
+        }
+
+        
+    }
+}
---
+++ CometTailSimulator/CometTailSimulator/Classes/PixelParticle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CometTailSimulator.Classes
+{
+    public class PixelParticle
+    {
+        private int x;
+        private int y;
+        private double size;
+
+        PixelParticle(int x, int y, double size)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public double Size { get => size; set => size = value; }
+    }
+}
